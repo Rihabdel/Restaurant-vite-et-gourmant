298e29006a111688f26d9c362b910fde523ae66d
Rihabdel <rihababdel@gmail.com>
2025-12-28 00:02:29 +0100

    carte de menu et modal de modification, de suppression et de details de menu

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,8 @@ import Route from "/Router/Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/menu", "Menu", "/pages/menu.html"),
+    new Route("/details", "Détails", "/pages/details.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
