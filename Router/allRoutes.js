b1d6cd11fb2223b38ba29a5c1a6265bf24c0ec8c
Rihabdel <rihababdel@gmail.com>
2026-01-03 20:40:39 +0100

    front de la page de profil et de mes commandes

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -7,6 +7,9 @@ export const allRoutes = [
     new Route("/details", "Détails", "/pages/details.html"),
     new Route("/connexion", "Connexion", "/pages/connexion.html"),
     new Route("/inscription", "Inscription", "/pages/inscription.html"),
+    new Route("/profil", "Profil", "/pages/profil.html"),
+    new Route("/monCompte", "Mon Compte", "/pages/profil.html"),
+    new Route("/order", "Mes commandes", "/pages/order.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
